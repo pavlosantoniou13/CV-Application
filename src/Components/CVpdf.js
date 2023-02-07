a2c410cfa6ff6eda496d0351838d1a484ab96370
pavlosantoniou13 <dinant11133@gmail.com>
2023-02-08 01:07:37 +0200

    A

--- src/Components/CVpdf.js
+++ src/Components/CVpdf.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function CVpdf() {
   return (
-    <div>CVpdf</div>
+    <div className='CV'>
+      <div>Name:</div>
+      </div>
   )
 }
---
+++ src/HomePage.js
@@ -0,0 +1,16 @@
+import GeneralInfo from './Components/GeneralInfo'
+import Education from './Components/Education'
+import Jobs from './Components/Jobs'
+
+import React from 'react'
+
+export default function HomePage() {
+
+  return (
+    <div>
+        <GeneralInfo  />
+        <Education />
+        <Jobs />
+    </div>
+  )
+}
